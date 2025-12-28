9ca318f94c887595c51f732dbc1d5a731817c691
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2025-12-28 12:03:56 +0000

    chore: initial setup with vite and tailwind 4 confirmed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    // We use h-screen to make sure the color fills the height
+    <div className="bg-red-600 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold italic">
+        Tailwind 4 is Live!
+      </h1>
+    </div>
+  )
+}
+
+export default App
